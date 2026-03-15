83e6185703e7b3f4fd5d7a9407f84057962ba9f1
ali99944 <alitarek99944@gmail.com>
2026-03-15 22:12:15 +0200

    clean up project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Home from "./page";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Ali Tarek — Fullstack Engineer | System Architect",
@@ -25,11 +13,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link type="image/png" sizes="16x16" rel="icon" href="/icons/port.png" />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         {children}
-        <Home />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      
+      <div className="bg-background text-foreground">
+        Welcome to my portfolio! am Ali Tarek
+      </div>
     </div>
   );
 }
